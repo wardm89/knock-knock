d2753d7abffd8dace6f3bccccc5a0ff557226d5b
Matthew Ward <wardm89@yahoo.com>
2023-06-15 11:57:38 -0400

    Added clerk auth

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+
+export default authMiddleware({
+  publicRoutes: ["/"],
+  afterAuth(auth, req, _evt) {
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+  },
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { ClerkProvider } from "@clerk/nextjs";
+
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
@@ -9,9 +11,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ClerkProvider {...pageProps}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ClerkProvider>
   );
 };
 
